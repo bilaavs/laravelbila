ef3198e70332ccf44d9ed62d1208962fc4e98b1a
“bilaavs” <nabilaramadhani796@gmail.com>
2025-02-04 14:00:39 +0700

    delete

--- resources/views/template/separate/genres.blade.php
+++ resources/views/template/separate/genres.blade.php
@@ -346,7 +346,6 @@
     <!-- Main Footer -->
     <footer class="main-footer">
       <div class="float-right d-none d-sm-inline">
-        Anything you want
       </div>
       <span class="copyright-text">
         <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
